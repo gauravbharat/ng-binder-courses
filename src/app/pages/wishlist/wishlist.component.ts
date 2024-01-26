ecd5c4d360b2b67fbe6d82b4b57a0345b92f00ed
Gaurav Bharat <gaurav.mendse@gmail.com>
2024-01-26 09:51:42 +0530

    show message when no wishlisted items

--- src/app/pages/wishlist/wishlist.component.ts
+++ src/app/pages/wishlist/wishlist.component.ts
@@ -2,13 +2,17 @@ import { Component, inject } from '@angular/core';
 import { BannerComponent } from '../../shared/components/banner/banner.component';
 import { CourseCardComponent } from '../../shared/components/course-card/course-card.component';
 import { Store } from '@ngrx/store';
-import { selectWishlistedCourses } from '../../state/user.selectors';
+import {
+  selectTotalWishlistedCourses,
+  selectWishlistedCourses,
+} from '../../state/user.selectors';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-wishlist',
   standalone: true,
-  imports: [CommonModule, BannerComponent, CourseCardComponent],
+  imports: [CommonModule, BannerComponent, CourseCardComponent, RouterModule],
   templateUrl: './wishlist.component.html',
   styleUrl: './wishlist.component.css',
 })
@@ -16,4 +20,5 @@ export class WishlistComponent {
   #store = inject(Store);
 
   wishlistedCourses$ = this.#store.select(selectWishlistedCourses);
+  totalWishlistedCourses$ = this.#store.select(selectTotalWishlistedCourses);
 }
